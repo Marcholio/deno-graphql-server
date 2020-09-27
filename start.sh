9dbf2cc40ee1bd24b6595ea11df5a15027d4a1da
Markus <markus.tyrkko@gmail.com>
2020-09-27 13:31:05 +0300

    Fix exports

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 curl -fsSL https://deno.land/x/install/install.sh | sh
 
-export DENO_INSTALL="/app/deno"
+export DENO_INSTALL="/app/.deno"
 export PATH="$DENO_INSTALL/bin:$PATH"
 
 deno --version
