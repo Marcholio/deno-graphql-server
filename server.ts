8cb40b421a20153b431f3ec4e3b5e5ca37fdc1a4
Markus <markus.tyrkko@gmail.com>
2020-09-27 13:22:15 +0300

    Simple server

---
+++ server.ts
@@ -0,0 +1,16 @@
+/**
+ * Simple web server copied from Deno examples:
+ * https://deno.land/manual/examples/http_server
+ */
+
+import { serve } from "./deps.ts";
+
+const server = serve({ hostname: "0.0.0.0", port: 8080 });
+console.log(`HTTP webserver running.  Access it at:  http://localhost:8080/`);
+
+for await (const request of server) {
+  let bodyContent = "Your user-agent is:\n\n";
+  bodyContent += request.headers.get("user-agent") || "Unknown";
+
+  request.respond({ status: 200, body: bodyContent });
+}
