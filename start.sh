a24446b518b15dc43715eea130d28a98ad001924
Markus <markus.tyrkko@gmail.com>
2020-09-27 13:38:47 +0300

    Use lock file in start script

--- start.sh
+++ start.sh
@@ -5,4 +5,4 @@ curl -fsSL https://deno.land/x/install/install.sh | sh
 export DENO_INSTALL="/app/.deno"
 export PATH="$DENO_INSTALL/bin:$PATH"
 
-deno run --allow-net --allow-env ./server.ts
+deno run --lock=lock.json --allow-net --allow-env ./server.ts
