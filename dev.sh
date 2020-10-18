c43dcf7f43fda2b5d0f406e151553090fa88b647
Markus <markus.tyrkko@gmail.com>
2020-10-18 19:50:42 +0300

    Added CORS headers

---
+++ dev.sh
@@ -0,0 +1,1 @@
+deno run --allow-net --allow-env --lock=lock.json --lock-write --watch --unstable ./server.ts
--- server.ts
+++ server.ts
@@ -1,8 +1,8 @@
 import { Application, applyGraphQL, Router } from "./deps.ts";
 
-import { typeDefs, resolvers } from "./graphql.ts";
+import { resolvers, typeDefs } from "./graphql.ts";
 
-const port = Number(Deno.env.get("PORT")) || 8080;
+const port = Number(Deno.env.get("PORT")) || 8000;
 
 const hostname = "0.0.0.0";
 
@@ -12,6 +12,19 @@ const GraphQLService = await applyGraphQL<Router>({
   Router,
   typeDefs,
   resolvers,
+});
+
+// CORS headers
+app.use(async (ctx, next) => {
+  ctx.response.headers.set(
+    "Access-Control-Allow-Origin",
+    Deno.env.get("MODE") === "production"
+      ? "https://marcholio.github.io/vue-todo"
+      : "http://localhost:8080",
+  );
+  ctx.response.headers.set("Content-Type", "application/json");
+  ctx.response.headers.set("Access-Control-Allow-Headers", "*");
+  await next();
 });
 
 app.use(GraphQLService.routes(), GraphQLService.allowedMethods());
