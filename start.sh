876aae6e01cf30d0166ce031cd40267d066e0f7b
Markus <markus.tyrkko@gmail.com>
2021-05-10 22:10:28 +0300

    No lock write

--- start.sh
+++ start.sh
@@ -5,4 +5,4 @@ curl -fsSL https://deno.land/x/install/install.sh | sh
 export DENO_INSTALL="/app/.deno"
 export PATH="$DENO_INSTALL/bin:$PATH"
 
-deno run --lock=lock.json --lock-write --allow-net --allow-env --allow-read=baseData.json ./server.ts
+deno run --lock=lock.json --allow-net --allow-env --allow-read=baseData.json ./server.ts
