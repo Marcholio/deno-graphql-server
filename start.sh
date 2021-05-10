772e97da47356fde5fca68b17c89a8528a00b7d4
Markus <markus.tyrkko@gmail.com>
2021-05-10 22:17:54 +0300

    Debugging

--- start.sh
+++ start.sh
@@ -5,4 +5,6 @@ curl -fsSL https://deno.land/x/install/install.sh | sh
 export DENO_INSTALL="/app/.deno"
 export PATH="$DENO_INSTALL/bin:$PATH"
 
+deno --version
+
 deno run --lock=lock.json --allow-net --allow-env --allow-read=baseData.json ./server.ts
