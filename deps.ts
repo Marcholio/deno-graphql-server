7642247fab726d9231408949b0c641141bb26332
Markus <markus.tyrkko@gmail.com>
2021-05-10 22:12:40 +0300

    Oak 7.4.0

--- deps.ts
+++ deps.ts
@@ -1,4 +1,4 @@
 export { serve } from "https://deno.land/std@0.95.0/http/server.ts";
-export { Application, Router } from "https://deno.land/x/oak/mod.ts";
+export { Application, Router } from "https://deno.land/x/oak@v7.4.0/mod.ts";
 export { applyGraphQL, gql } from "http://deno.land/x/oak_graphql/mod.ts";
 export { v4 as uuidV4 } from "https://deno.land/std@0.95.0/uuid/mod.ts";
